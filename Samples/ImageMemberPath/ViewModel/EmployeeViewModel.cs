b8724f7079e2fe824a3dc5003f093e82343361a4
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2020-06-11 14:53:05 +0530

    Samples added
    Customization and getting started samples added

---
+++ Samples/ImageMemberPath/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageMemberPath
+{
+
+    public class EmployeeViewModel
+    {
+        private List<Employee> employees;
+        public List<Employee> Employees
+        {
+            get { return employees; }
+
+            set { employees = value; }
+        }
+
+        public EmployeeViewModel()
+        {
+            Employees = new List<Employee>();
+            Employees.Add(new Employee { Name = "Lucas", Email = "lucas@syncfusion.com", Image = "a0.png" });
+            Employees.Add(new Employee { Name = "James", Email = "james@syncfusion.com", Image = "a1.png" });
+            Employees.Add(new Employee { Name = "Jacob", Email = "jacob@syncfusion.com", Image = "a2.png" });
+        }
+    }
+}
---
+++ Samples/Single-and-multiple-selection/ViewModel/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_and_multiple_selection
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+}
