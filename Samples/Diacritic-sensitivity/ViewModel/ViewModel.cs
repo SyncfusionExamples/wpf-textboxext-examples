7397458f4af5cdf58a6153c253cfd6a641309ad1
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2020-06-11 14:50:26 +0530

    Samples added
    Customization samples committed to samples

---
+++ Samples/Diacritic-sensitivity/ViewModel/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diacritic_sensitivity
+{
+    public class ViewModel
+    {
+        private List<Model> diacriticCollenction;
+        public List<Model> DiacriticCollenction
+        {
+            get { return diacriticCollenction; }
+
+            set { diacriticCollenction = value; }
+        }
+
+        public ViewModel()
+        {
+            DiacriticCollenction = new List<Model>();
+            DiacriticCollenction.Add(new Model { Item = "Hów tó gâin wéight?" });
+            DiacriticCollenction.Add(new Model { Item = "Hów tó drâw ân éléphânt" });
+            DiacriticCollenction.Add(new Model { Item = "Whéré cân I buy â câmérâ?" });
+            DiacriticCollenction.Add(new Model { Item = "Guidé mé âll thé wây" });
+            DiacriticCollenction.Add(new Model { Item = "Hów tó mâké â câké?" });
+            DiacriticCollenction.Add(new Model { Item = "Sây, Hélló Wórld!" });
+            DiacriticCollenction.Add(new Model { Item = "Hów tó mâké â róbót?" });
+            DiacriticCollenction.Add(new Model { Item = "Whât timé nów in Indiâ?" });
+        }
+    }
+}
