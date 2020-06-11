dc4bb630f8233c08fd1ac939d1c9bacbbb04533d
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2020-06-11 14:55:15 +0530

    Samples added
    samples added to TextBoxExt control

---
+++ Samples/TextHighlightMode/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextHighlightMode
+{
+
+    public class EmployeeViewModel
+    {
+        private List<Employee> employees;
+        public List<Employee> Employees
+        {
+            get { return employees; }
+
+            set { employees = value; }
+        }
+
+        public EmployeeViewModel()
+        {
+            Employees = new List<Employee>();
+            Employees.Add(new Employee() { Name = "Eric", Email = "Eric@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "James", Email = "James@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "Jacob", Email = "Jacob@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "Jackson", Email = "Jackson@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "Lucas", Email = "Lucas@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "Mark", Email = "Mark@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "Aldan", Email = "Aldan@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "Aldrin", Email = "Aldrin@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "Alan", Email = "Alan@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "Aaron", Email = "Aaron@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "Sebastian", Email = "Sebastian@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "Gabriella", Email = "Gabriella@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "Alexander", Email = "Alexander@syncfusion.com" });          
+        }
+    }
+}
---
+++ Samples/Textbox-customization/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Textbox_customization
+{
+
+    public class EmployeeViewModel
+    {
+        private List<Employee> employees;
+        public List<Employee> Employees
+        {
+            get { return employees; }
+
+            set { employees = value; }
+        }
+
+        public EmployeeViewModel()
+        {
+            Employees = new List<Employee>();
+            Employees.Add(new Employee() { Name = "Eric", Email = "Eric@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "James", Email = "James@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "Jacob", Email = "Jacob@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "Jackson", Email = "Jackson@syncfusion.com" });
+            Employees.Add(new Employee() { Name = "Lucas", Email = "Lucas@syncfusion.com" });         
+        }
+    }
+}
