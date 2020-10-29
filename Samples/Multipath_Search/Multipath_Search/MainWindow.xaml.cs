2ce06f713713def73e518614881df280a90519fe
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2020-10-29 17:34:03 +0530

    Added Multi path search sample

---
+++ Samples/Multipath_Search/Multipath_Search/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Multipath_Search
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            autoComplete.Filter = CustomFilter;
+        }
+        private bool CustomFilter(string search, object item)
+        {
+            var model = item as Model;
+            if (model != null)
+            {
+                if ((model.Name.ToLower().Contains(search.ToLower())) || ((model.ID).ToString().ToLower().Contains(search.ToLower())))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Samples/Multipath_Search/Multipath_Search/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multipath_Search
+{
+    public class ViewModel
+    {
+        private ObservableCollection<Model> employeeCollection;
+        public ObservableCollection<Model> EmployeeCollection
+        {
+            get { return employeeCollection; }
+            set { employeeCollection = value; }
+        }
+        public ViewModel()
+        {
+            employeeCollection = new ObservableCollection<Model>();
+            employeeCollection.Add(new Model() { ID = 1, Name = "Eric" });
+            employeeCollection.Add(new Model() { ID = 2, Name = "James" });
+            employeeCollection.Add(new Model() { ID = 3, Name = "Jacob" });
+            employeeCollection.Add(new Model() { ID = 4, Name = "Lucas" });
+            employeeCollection.Add(new Model() { ID = 5, Name = "Mark" });
+            employeeCollection.Add(new Model() { ID = 6, Name = "Aldan" });
+            employeeCollection.Add(new Model() { ID = 7, Name = "Aldrin" });
+            employeeCollection.Add(new Model() { ID = 8, Name = "Alan" });
+            employeeCollection.Add(new Model() { ID = 9, Name = "Aaron" });
+        }
+    }
+}
